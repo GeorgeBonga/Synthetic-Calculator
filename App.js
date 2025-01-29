b59c08ba9a815dc019f1b3fa6235ad0cfae51aa4
George Bonga <49430815+GeorgeBonga@users.noreply.github.com>
2025-01-29 15:52:19 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,14 +1,5 @@
 import React from "react";
 import { View, Text, StatusBar, AppState } from "react-native";
 
-import DrawerNavigator from "./src/navigation/Drawernavigator";
-const App = () => {
-  return (
-    <View style={{ flex: 1 }}>
-      <StatusBar barStyle="light-content" backgroundColor="#FF6666" />
-      <DrawerNavigator />
-    </View>
-  );
-};
 
 export default App;
